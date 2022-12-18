d10f550ef2c56e3b0afb3a2fae22c215e68c159b
NihilLeaf <nihilleaf@gmail.com>
2022-12-18 19:54:19 -0300

    Iniciando Projeto

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h1>Teste</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data() {
+    return {
+      tasks: [
+        {name: 'lavar louça', pending: false},
+        {name: 'Comprar Sapato', pending: true}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: 'Ubuntu', sans-serif;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-image: linear-gradient(to right, #0d1226,
+      #3d2e22);
+  overflow-x: hidden;
+  color: #bf7329;
+}
+</style>
